bf0efd9852a994f4a022adbcdb22c9363e0f42ae
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-26 11:05:50 +0000

    fourth unit test passed

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -9,7 +9,7 @@ describe('App.vue', () => {
     wrapper.vm.add('5');
     expect(wrapper.vm.runningTotal).to.equal(9)
   })
-})
+
 it('should add 1 to 4 = 5', () => {
   const wrapper = shallowMount(App)
   wrapper.vm.previousTotal = 4
@@ -22,4 +22,20 @@ it('should subtract 4 from 7 and get 3', () => {
   wrapper.vm.previousTotal = 7
   wrapper.vm.subtract('4');
   expect(wrapper.vm.runningTotal).to.equal(3)
+})
+
+it('should multiply 3 by 5 and get 15', () => {
+  const wrapper = shallowMount(App)
+  wrapper.vm.previousTotal = 3
+  wrapper.vm.multiply('5');
+  expect(wrapper.vm.runningTotal).to.equal(15)
+})
+
+it('should divide 21 by 7 and get 3', () => {
+  const wrapper = shallowMount(App)
+  wrapper.vm.previousTotal = 21
+  wrapper.vm.divide('7');
+  expect(wrapper.vm.runningTotal).to.equal(3)
+})
+
 })
