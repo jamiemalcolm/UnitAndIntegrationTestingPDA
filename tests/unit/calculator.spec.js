2798e88b2d66058b27de33ee60f85f3ac10f3760
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-26 11:43:26 +0000

    last unit test passed

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -40,22 +40,34 @@ it('should divide 21 by 7 and get 3', () => {
 
 it('should concatenate multiple number button clicks', () => {
   const wrapper = shallowMount(App)
-  wrapper.vm.numberClick('2')
-  wrapper.vm.numberClick('2') 
+  wrapper.vm.numberClick('2');
+  wrapper.vm.numberClick('2') ;
   expect(wrapper.vm.runningTotal).to.equal(22)
 })
 
 it('should chain multiple operations together', () => {
   const wrapper = shallowMount(App)
-  wrapper.vm.numberClick('2')
-  wrapper.vm.numberClick('4')
-  wrapper.vm.operatorClick('+')
-  wrapper.vm.numberClick('12')
-  wrapper.vm.operatorClick('-')
-  wrapper.vm.numberClick('3')
-  wrapper.vm.operatorClick('=')
+  wrapper.vm.numberClick('2');
+  wrapper.vm.numberClick('4');
+  wrapper.vm.operatorClick('+');
+  wrapper.vm.numberClick('12');
+  wrapper.vm.operatorClick('-');
+  wrapper.vm.numberClick('3');
+  wrapper.vm.operatorClick('=');
   expect(wrapper.vm.runningTotal).to.equal(33)
-
-
 })
+
+it('should clear the running total without affecting the calculation', () => {
+  const wrapper = shallowMount(App)
+  wrapper.vm.numberClick('2');
+  wrapper.vm.numberClick('4');
+  wrapper.vm.operatorClick('+');
+  wrapper.vm.numberClick('12');
+  wrapper.vm.operatorClick('-');
+  wrapper.vm.numberClick('3');
+  wrapper.vm.clearClick();
+  wrapper.vm.operatorClick('=');
+  expect(wrapper.vm.runningTotal).to.equal(36)
+})
+
 })
